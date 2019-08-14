0a72cdd458c6cdaeea2d95dedf237d31ae1e3992
Aleh Haidukevich <Aleh.Haidukevich@itechart-group.com>
2019-08-14 11:57:18 +0300

    get all common data

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 // Requiring two of our dependencies
 const request = require('request');
 const cheerio = require('cheerio');
-let href = [];
+let data = [];
 // Requesting to the website
 request(
   'https://domovita.by/minsk/1-room-flats/rent',
   (error, response, html) => {
-    // Checking that there is no errors and the response code is correct
     if (!error && response.statusCode === 200) {
-      // Declaring cheerio for future usage
       const $ = cheerio.load(html, {
         xml: {
           normalizeWhitespace: true
         }
       });
 
-      // Looking at the inspector or source code we will select the following id value
       const siteHeading = $('.found_content.last-view-data')
         .find('div > a')
         .each((index, element) => {
@@ -28,7 +25,7 @@ request(
             )
             .get();
 
-          const flatData = $(element).find('.found_full div');
+          const flatData = Array.from($(element).find('.found_full > div.row'));
 
           const address = $(flatData[0])
             .children('.col-md-8')
@@ -41,9 +38,24 @@ request(
             })
             .get();
 
-          console.log('---------------------------------');
-          console.log(address);
+          const price = $(flatData[0])
+            .find('div.price.mb-2.mt-10 ')
+            .first()
+            .text();
+
+          if (address.length) {
+            data.push({
+              images,
+              city: address[0].split(',')[1].trim(),
+              street: address[0].split(',')[2].trim(),
+              apartment: address[0].split(',')[3].trim(),
+              place: address[1],
+              subway: address[2],
+              price: price.split('&')[0]
+            });
+          }
         });
     }
+    console.log(data);
   }
 );
